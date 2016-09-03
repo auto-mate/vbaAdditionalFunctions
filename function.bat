f7681f2c7f8c07932c9d0fbdd23ee1891efdb812
auto-mate <aandaharper@gmail.com>
2016-09-03 16:17:49 +0100

    Complete
    Add additional fake code to complete

--- function.bat
+++ function.bat
@@ -1,4 +1,13 @@
-Function VlookupEx(Lrange As Range,Offset)
+Function VlookupEx(lookupVal,Lrange As Range,Offset)
+
   dim cellColl as Range
+
   VlookupEx=0
+  for each cellColl in Lrange
+    if cellColl.value=lookupVal then
+        VlookupEx=cellColl.(0,Offset).value
+      exit for
+    endif
+  next
+  
 End Function
